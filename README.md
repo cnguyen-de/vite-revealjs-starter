e066bd49d67ac39832aaf829a347084ea598f91f
Chi Nguyen <minhchi.nguyen@novatec-gmbh.de>
2022-09-09 15:41:36 +0200

    Create scaffold revealjs & vite

---
+++ README.md
@@ -0,0 +1,7 @@
+# Install dependencies
+
+`pnpm install`
+
+# Run local dev server
+
+`pnpm run dev`
---
+++ main.js
@@ -0,0 +1,9 @@
+import './styles/style.css'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
+
+import './styles/nt.css'
+import Reveal from 'reveal.js'
+
+const deck = new Reveal()
+deck.initialize({ hash: true, slideNumber: true })
